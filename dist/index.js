7955238ba44db5909fbdf658a8477f3855f824e0
gregor <gregor.aubrey.uk@gmail.com>
2023-07-15 10:42:49 +0100

    working on axios and cheerio to scrap spanish dict

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 console.log("This is the start of index.ts");
+const axios = require("axios");
+const cheerio = require("cheerio");
+function getDefinition(word) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = `https://www.spanishdict.com/translate/${word}`;
+        try {
+            const response = yield axios.get(url, {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+                }
+            });
+            const $ = cheerio.load(response.data);
+            const definition = $("body").text();
+            const firstIndex = definition.indexOf(word);
+            const startIndex = definition.indexOf(word, firstIndex + 4);
+            const endIndex = definition.indexOf("Copyright © Curiosity Media");
+            const actualDefinition = definition.slice(startIndex, endIndex);
+            return actualDefinition;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+}
+getDefinition("cuajar").then(console.log);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
 console.log("This is the start of index.ts");
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+async function getDefinition(word: string) {
+    const url = `https://www.spanishdict.com/translate/${word}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
+            }
+        });        
+        const $ = cheerio.load(response.data);
+        const definition = $("body").text();
+        const firstIndex = definition.indexOf(word);
+        const startIndex = definition.indexOf(word, firstIndex + 4);
+        const endIndex = definition.indexOf("Copyright © Curiosity Media");
+        const actualDefinition = definition.slice(startIndex, endIndex);
+        return actualDefinition;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+getDefinition("cuajar").then(console.log)
